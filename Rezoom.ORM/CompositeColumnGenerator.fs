5f30bc970e9ed564b5e15648d2c11bfaeb8ef4e1
Robert Peele <robert.s.peele@gmail.com>
2016-08-03 00:16:04 -0400

    Get recursive single composites working.

--- Rezoom.ORM/CompositeColumnGenerator.fs
+++ Rezoom.ORM/CompositeColumnGenerator.fs
@@ -17,46 +17,73 @@ type private CompositeColumnGenerator(builder, column, composite : Composite) =
     let mutable entReader = null
     override __.DefineConstructor() =
         entReader <- builder.DefineField("_c_r_" + column.Name, entReaderType, FieldAttributes.Private)
-        cil {
-            yield dup
-            yield call0 (staticTemplate.GetMethod("Template"))
-            yield call1 (entTemplate.GetMethod("CreateReader"))
-            yield stfld entReader
-        }
+        zero
     override __.DefineProcessColumns() =
         cil {
-            yield dup
-            yield ldfld entReader
+            let! ncase = deflabel // if submap is null
+            let! sub = deflocal typeof<ColumnMap>
             yield ldarg 1 // column map
             yield ldstr column.Name
             yield call2 ColumnMap.SubMapMethod
-            yield callvirt2'void (entReaderType.GetMethod("ProcessColumns"))
+            yield dup
+            yield stloc sub
+            yield brfalse's ncase
+            yield cil {
+                yield dup
+                yield call0 (staticTemplate.GetMethod("Template"))
+                yield callvirt1 (entTemplate.GetMethod("CreateReader"))
+                yield dup
+                yield ldloc sub
+                yield callvirt2'void (entReaderType.GetMethod("ProcessColumns"))
+                yield stfld entReader
+            }
+            yield mark ncase
         }
     override __.DefineImpartKnowledgeToNext() =
         cil {
+            let! ncase = deflabel
             yield ldarg 0
             yield ldfld entReader
-            yield ldarg 1
-            yield castclass builder
-            yield ldfld entReader
-            yield callvirt2'void (entReaderType.GetMethod("ImpartKnowledgeToNext"))
+            yield brfalse's ncase
+            yield cil {
+                yield ldarg 0
+                yield ldfld entReader
+                yield ldarg 1
+                yield castclass builder
+                yield ldfld entReader
+                yield callvirt2'void (entReaderType.GetMethod("ImpartKnowledgeToNext"))
+            }
+            yield mark ncase
         }
     override __.DefineRead(skipOnes) =
         cil {
+            let! ncase = deflabel
             yield dup
             yield ldfld entReader
-            yield ldarg 1
-            yield callvirt2'void (entReaderType.GetMethod("Read"))
+            yield brfalse's ncase
+            yield cil {
+                yield dup
+                yield ldfld entReader
+                yield ldarg 1
+                yield callvirt2'void (entReaderType.GetMethod("Read"))
+            }
+            yield mark ncase
         }
     override __.RequiresSelfReferenceToPush = requiresSelf
     override __.DefinePush(self) =
         cil {
+            let! ncase = deflabel
             yield ldarg 0
             yield ldfld entReader
-            if requiresSelf then
-                yield dup
-                yield ldloc self
-                if output.IsValueType then yield box'val output
-                yield callvirt2'void (entReaderType.GetMethod("SetQueryParent"))
-            yield callvirt1 (entReaderType.GetMethod("ToEntity"))
+            yield dup
+            yield brfalse's ncase
+            yield cil {
+                if requiresSelf then
+                    yield dup
+                    yield ldloc self
+                    if output.IsValueType then yield box'val output
+                    yield callvirt2'void (entReaderType.GetMethod("SetQueryParent"))
+                yield callvirt1 (entReaderType.GetMethod("ToEntity"))
+            }
+            yield mark ncase
         }
