36f3c1d83e49abb031f059928df0fe10b99dbde9
Robert Peele <robert.s.peele@gmail.com>
2016-09-17 20:03:36 -0400

    Add more unit tests.

--- SQLow.Test/TestTypeInference.fs
+++ SQLow.Test/TestTypeInference.fs
@@ -20,7 +20,67 @@ type TestTypeInference() =
     [<TestMethod>]
     member __.TestSimpleSelect() =
         let cmd = zeroModel |> Command.ofSql @"
-            create table Users(id int, name nvarchar(128), email nvarchar(128));
+            create table Users(id int primary key, name nvarchar(128), email nvarchar(128));
             select * from Users
         "
         printfn "%A" cmd
+        Assert.AreEqual(0, cmd.Parameters.Count)
+        Assert.AreEqual(1, cmd.ResultSets.Count)
+        let cs = cmd.ResultSets.[0].Columns
+        Assert.IsTrue(cs.[0].PrimaryKey)
+        Assert.AreEqual(Name("id"), cs.[0].ColumnName)
+        Assert.AreEqual({ Nullable = true; Type = IntegerType }, cs.[0].ColumnType)
+        Assert.IsFalse(cs.[1].PrimaryKey)
+        Assert.AreEqual(Name("name"), cs.[1].ColumnName)
+        Assert.AreEqual({ Nullable = true; Type = StringType }, cs.[1].ColumnType)
+        Assert.IsFalse(cs.[2].PrimaryKey)
+        Assert.AreEqual(Name("email"), cs.[2].ColumnName)
+        Assert.AreEqual({ Nullable = true; Type = StringType }, cs.[2].ColumnType)
+
+    [<TestMethod>]
+    member __.TestSimpleSelectWithParameter() =
+        let cmd = zeroModel |> Command.ofSql @"
+            create table Users(id int primary key, name nvarchar(128), email nvarchar(128));
+            select * from Users u
+            where u.id = @id
+        "
+        printfn "%A" cmd
+        Assert.AreEqual(1, cmd.Parameters.Count)
+        Assert.AreEqual
+            ( (NamedParameter ('@', Name("id")), { Nullable = true; Type = IntegerType })
+            , cmd.Parameters.[0])
+        Assert.AreEqual(1, cmd.ResultSets.Count)
+        let cs = cmd.ResultSets.[0].Columns
+        Assert.IsTrue(cs.[0].PrimaryKey)
+        Assert.AreEqual(Name("id"), cs.[0].ColumnName)
+        Assert.AreEqual({ Nullable = true; Type = IntegerType }, cs.[0].ColumnType)
+        Assert.IsFalse(cs.[1].PrimaryKey)
+        Assert.AreEqual(Name("name"), cs.[1].ColumnName)
+        Assert.AreEqual({ Nullable = true; Type = StringType }, cs.[1].ColumnType)
+        Assert.IsFalse(cs.[2].PrimaryKey)
+        Assert.AreEqual(Name("email"), cs.[2].ColumnName)
+        Assert.AreEqual({ Nullable = true; Type = StringType }, cs.[2].ColumnType)
+
+    [<TestMethod>]
+    member __.TestSimpleSelectWithParameterNotNull() =
+        let cmd = zeroModel |> Command.ofSql @"
+            create table Users(id int primary key not null, name nvarchar(128), email nvarchar(128));
+            select * from Users u
+            where u.id = @id
+        "
+        printfn "%A" cmd
+        Assert.AreEqual(1, cmd.Parameters.Count)
+        Assert.AreEqual
+            ( (NamedParameter ('@', Name("id")), { Nullable = false; Type = IntegerType })
+            , cmd.Parameters.[0])
+        Assert.AreEqual(1, cmd.ResultSets.Count)
+        let cs = cmd.ResultSets.[0].Columns
+        Assert.IsTrue(cs.[0].PrimaryKey)
+        Assert.AreEqual(Name("id"), cs.[0].ColumnName)
+        Assert.AreEqual({ Nullable = false; Type = IntegerType }, cs.[0].ColumnType)
+        Assert.IsFalse(cs.[1].PrimaryKey)
+        Assert.AreEqual(Name("name"), cs.[1].ColumnName)
+        Assert.AreEqual({ Nullable = true; Type = StringType }, cs.[1].ColumnType)
+        Assert.IsFalse(cs.[2].PrimaryKey)
+        Assert.AreEqual(Name("email"), cs.[2].ColumnName)
+        Assert.AreEqual({ Nullable = true; Type = StringType }, cs.[2].ColumnType)
