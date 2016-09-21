07a6603f0d05c396a2959a6d09b7403d3ca5bf83
Robert Peele <robert.s.peele@gmail.com>
2016-09-20 20:20:22 -0400

    Add constructor generation for generated command types.

--- Rezoom.ORM.SQLProvider/Backend.fs
+++ Rezoom.ORM.SQLProvider/Backend.fs
@@ -4,7 +4,10 @@ open System.Collections.Generic
 open Rezoom.ORM
 open SQLow
 
+type IParameterIndexer =
+    abstract member ParameterIndex : parameter : BindParameter -> int
+
 type IBackend =
     abstract member Builtin : DatabaseBuiltin
     abstract member MapPrimitiveType : ColumnType -> Type
-    abstract member ToCommandFragments : Stmt IReadOnlyList -> CommandFragment IReadOnlyList
+    abstract member ToCommandFragments : IParameterIndexer * Stmt IReadOnlyList -> CommandFragment IReadOnlyList
--- Rezoom.ORM.SQLProvider/TypeGeneration.fs
+++ Rezoom.ORM.SQLProvider/TypeGeneration.fs
@@ -4,8 +4,9 @@ open System.Collections.Generic
 open System.IO
 open System.Text.RegularExpressions
 open System.Reflection
-open Microsoft.FSharp.Core.CompilerServices
-open Microsoft.FSharp.Quotations
+open FSharp.Core.CompilerServices
+open FSharp.Quotations
+open FSharp.Reflection
 open ProviderImplementation.ProvidedTypes
 open Rezoom.ORM
 open SQLow
@@ -19,6 +20,29 @@ type GenerateType =
         TypeName : string
         Command : CommandWithEffect
     }
+
+let private parameterIndexer (pars : BindParameter seq) =
+    let dict =
+        pars |> Seq.indexed |> Seq.map (fun (a, b) -> (b, a)) |> dict
+    { new IParameterIndexer with
+        member __.ParameterIndex(par) = dict.[par]
+    }
+
+let private localNameCase, private commandTextCase, private parameterCase =
+    let cases = FSharpType.GetUnionCases(typeof<CommandFragment>)
+    ( cases |> Array.find (fun c -> c.Name = "LocalName")
+    , cases |> Array.find (fun c -> c.Name = "CommandText")
+    , cases |> Array.find (fun c -> c.Name = "Parameter")
+    )
+
+let private toFragmentExpr (fragment : CommandFragment) =
+    match fragment with
+    | LocalName n -> Expr.NewUnionCase(localNameCase, [ Quotations.Expr.Value(n) ])
+    | CommandText t -> Expr.NewUnionCase(commandTextCase, [ Quotations.Expr.Value(t) ])
+    | Parameter i -> Expr.NewUnionCase(parameterCase, [ Quotations.Expr.Value(i) ])
+
+let private toFragmentArrayExpr (fragments : CommandFragment IReadOnlyList) =
+    Expr.NewArray(typeof<CommandFragment>, fragments |> Seq.map toFragmentExpr |> Seq.toList)
 
 /// Lowercase initial uppercase characters.
 let private toCamelCase (str : string) =
@@ -55,7 +79,7 @@ let rec private generateRowTypeFromColumns (model : UserModel) name (columnMap :
     for KeyValue(name, (_, column)) in columnMap.Columns do
         addField column.PrimaryKey name <| model.Backend.MapPrimitiveType(column.ColumnType)
     for KeyValue(name, subMap) in columnMap.SubMaps do
-        let subTy = generateRowTypeFromColumns model name subMap
+        let subTy = generateRowTypeFromColumns model (name + "Row") subMap
         ty.AddMember(subTy)
         addField false name subTy
     let ctorParams = [ for camel, field in fields -> ProvidedParameter(camel, field.FieldType) ]
@@ -71,18 +95,57 @@ let rec private generateRowTypeFromColumns (model : UserModel) name (columnMap :
     ty.AddMember(ctor)
     ty
 
-let private generateRowType (name : string) (model : UserModel) (query : SchemaQuery) =
+let private generateRowType (model : UserModel) (name : string) (query : SchemaQuery) =
     CompileTimeColumnMap.Parse(query.Columns)
     |> generateRowTypeFromColumns model name
 
-let generateType generate =
-    let baseType = typeof<obj> // TODO pick base command type based on result sets
-    let genTy =
-        ProvidedTypeDefinition
-            ( generate.Assembly
-            , generate.Namespace
-            , generate.TypeName
-            , Some baseType
-            , IsErased = false
-            )
-    genTy
+let private generateCtor (baseCtor : ConstructorInfo) (generate : GenerateType) =
+    let backend = generate.UserModel.Backend
+    let parameters = generate.Command.Effect.Parameters
+    let indexer = parameterIndexer (parameters |> Seq.map fst)
+    let fragments = backend.ToCommandFragments(indexer, generate.Command.Statements)
+    let parameters =
+        [ for NamedParameter name, ty in parameters ->
+            ProvidedParameter(name.Value, generate.UserModel.Backend.MapPrimitiveType(ty))
+        ]
+    let ctor = ProvidedConstructor(parameters)
+    ctor.BaseConstructorCall <-
+        function
+        | this :: args ->
+            let arr = Expr.NewArray(typeof<obj>, args |> List.map (fun e -> Expr.Coerce(e, typeof<obj>)))
+            let frags = toFragmentArrayExpr fragments
+            baseCtor, [ frags; arr ]
+        | _ ->
+            failwith "Invalid base constructor call argument list"
+    ctor.InvokeCode <- fun _ -> <@@ () @@>
+    ctor
+
+let generateType (generate : GenerateType) =
+    let rowTypes, baseTy =
+        let genRowType = generateRowType generate.UserModel
+        match generate.Command.Effect.ResultSets |> Seq.toList with
+        | [] -> [], typeof<Rezoom.ORM.Command0<unit>>
+        | [ resultSet ] ->
+            let rowType = genRowType "Row" resultSet
+            [ rowType ],
+                typedefof<Rezoom.ORM.Command1<_>>.MakeGenericType(rowType)
+        | [ resultSet1; resultSet2 ] ->
+            let rowType1 = genRowType "Row1" resultSet1
+            let rowType2 = genRowType "Row2" resultSet2
+            [ rowType1; rowType2 ],
+                typedefof<Rezoom.ORM.Command2<_, _>>.MakeGenericType(rowType1, rowType2)
+        | [ resultSet1; resultSet2; resultSet3 ] ->
+            let rowType1 = genRowType "Row1" resultSet1
+            let rowType2 = genRowType "Row2" resultSet2
+            let rowType3 = genRowType "Row3" resultSet3
+            [ rowType1; rowType2; rowType3 ],
+                typedefof<Rezoom.ORM.Command3<_, _, _>>.MakeGenericType(rowType1, rowType2, rowType3)
+        | _ ->
+            failwithf "Too many (%d) result sets from command." generate.Command.Effect.ResultSets.Count
+    let provided =
+        ProvidedTypeDefinition(generate.Assembly, generate.Namespace, generate.TypeName, Some baseTy, IsErased = false)
+    provided.AddMembers rowTypes
+    let baseCtor = baseTy.GetConstructor([| typeof<CommandFragment IReadOnlyList>; typeof<obj IReadOnlyList> |])
+    if isNull baseCtor then failwith "No matching base ctor"
+    provided.AddMember <| generateCtor baseCtor generate
+    provided
