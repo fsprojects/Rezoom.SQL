cbf89576ba0841ce1f7412d2d91daee8af5a7b4c
Robert Peele <robert.s.peele@gmail.com>
2016-10-14 19:32:33 -0400

    Clean up provider test script migrations.

--- StaticQL.Provider/user-migrations/V1.sql
+++ StaticQL.Provider/user-migrations/V1.sql
@@ -4,15 +4,15 @@ create table Users
        , Email string(128)
        , Password binary(64)
        , Salt binary(64)
-       )
+       );
 
 create Table Groups
-       ( Id not null primary key
+       ( Id int not null primary key
        , Name string(128)
-       )
+       );
 
 create table UserGroupMaps
-       ( UserId primary key references Users(Id)
-       , GroupId primary key references Groups(Id)
-       )
+       ( UserId int not null primary key references Users(Id)
+       , GroupId int not null primary key references Groups(Id)
+       );
 
