a9b4c2f0253fb511b00a90052ad5980321051b5c
Robert Peele <robert.s.peele@gmail.com>
2016-10-31 18:47:35 -0400

    Correct equality for command argument.

--- Rezoom.SQL.Mapping/CommandExtensions.fs
+++ Rezoom.SQL.Mapping/CommandExtensions.fs
@@ -1,26 +1,37 @@
 ﻿[<AutoOpen>]
 module Rezoom.SQL.Mapping.CommandExtensions
 open System
+open System.Collections.Generic
 open System.Data
 open System.Data.Common
 open Rezoom
 
-type Command<'a> with
-    member this.ExecuteAsync(conn : DbConnection) =
-        CommandBatch(conn).Batch(this)()
-    member this.Execute(conn : DbConnection) =
-        CommandBatch(conn).Batch(this)()
+type CommandErrandArgument(parameters : (obj * DbType) IReadOnlyList) =
+    member __.Parameters = parameters
+    member __.Equals(other : CommandErrandArgument) =
+        Seq.forall2 (=) parameters other.Parameters
+    override __.GetHashCode() =
+        let mutable h = 0
+        for par, _ in parameters do
+            h <- ((h <<< 5) + h) ^^^ par.GetHashCode()
+        h
+    override this.Equals(other : obj) =
+        match other with
+        | :? CommandErrandArgument as other -> this.Equals(other)
+        | _ -> false
 
 type CommandErrand<'a>(command : Command<'a>) =
     inherit AsynchronousErrand<'a>()
+    let cacheArgument = CommandErrandArgument(command.Parameters)
     override __.CacheInfo = command.CacheInfo
-    override __.CacheArgument =
-        let parameters = command.Parameters
-        { new obj() with
-            override __.GetHashCode() = 0
-                
-        }
+    override __.CacheArgument = box cacheArgument 
     override __.SequenceGroup = box typeof<Command>
     override __.Prepare(cxt) =
         let batch = cxt.GetService<CommandBatch>()
         batch.Batch(command)
+
+type Command<'a> with
+    member this.ExecutePlan() =
+        CommandErrand(this) |> Plan.ofErrand
+    member this.ExecuteAsync(conn : DbConnection) =
+        CommandBatch(conn).Batch(this)()
