60c2191735eb86618dc18cecb0f4ccdca7e50fb1
Robert Peele <robert.s.peele@gmail.com>
2016-11-11 00:18:47 -0500

    Add tests for new insert/update type errors.

--- Rezoom.SQL.Test/TestTypeErrors.fs
+++ Rezoom.SQL.Test/TestTypeErrors.fs
@@ -24,3 +24,34 @@ let ``unioned queries must have the same number of columns`` () =
             union all
             select 4, 5
         """
+
+[<Test>]
+let ``updates must set actual columns`` () =
+    expectError "No such column to set: ``Nane``"
+        """
+            update Users
+            set Id = 1, Nane = ''
+            where Id > 5
+        """
+
+[<Test>]
+let ``updated column types must match`` () =
+    expectError "The types INT and STRING cannot be unified"
+        """
+            update Users
+            set Id = 'five'
+        """
+
+[<Test>]
+let ``inserted column types must match`` () =
+    expectError "The types STRING and INT cannot be unified"
+        """
+            insert into Users(Id, Name) values ('one', 'jim')
+        """
+
+[<Test>]
+let ``inserted columns must exist`` () =
+    expectError "No such column: ``Goober``"
+        """
+            insert into Users(Goober, Booger) values ('one', 'jim')
+        """
