a4f64f022d09ba04253c9841ef832b613d7729d4
Robert Peele <robert.s.peele@gmail.com>
2017-01-06 20:26:15 -0500

    Add test for "is" forcing null.

--- Rezoom.SQL.Test/TestTypeInference.fs
+++ Rezoom.SQL.Test/TestTypeInference.fs
@@ -58,6 +58,30 @@ let ``simple select with parameter`` () =
     Assert.AreEqual({ Nullable = true; Type = StringType }, cs.[0].Expr.Info.Type)
 
 [<Test>]
+let ``simple select with parameter nullable id`` () =
+    let cmd = CommandEffect.OfSQL(zeroModel, "anonymous", @"
+        create table Users(id int primary key, name string(128), email string(128));
+        select * from Users u
+        where u.id is @id
+    ")
+    Assert.AreEqual(1, cmd.Parameters.Count)
+    Assert.AreEqual
+        ( (NamedParameter (Name("id")), { Nullable = true; Type = IntegerType Integer32 })
+        , cmd.Parameters.[0])
+    let results = cmd.ResultSets() |> toReadOnlyList
+    Assert.AreEqual(1, results.Count)
+    let cs = results.[0].Columns
+    Assert.IsTrue(cs.[1].Expr.Info.PrimaryKey)
+    Assert.AreEqual(Name("id"), cs.[1].ColumnName)
+    Assert.AreEqual({ Nullable = true; Type = IntegerType Integer32 }, cs.[1].Expr.Info.Type)
+    Assert.IsFalse(cs.[2].Expr.Info.PrimaryKey)
+    Assert.AreEqual(Name("name"), cs.[2].ColumnName)
+    Assert.AreEqual({ Nullable = true; Type = StringType }, cs.[2].Expr.Info.Type)
+    Assert.IsFalse(cs.[0].Expr.Info.PrimaryKey)
+    Assert.AreEqual(Name("email"), cs.[0].ColumnName)
+    Assert.AreEqual({ Nullable = true; Type = StringType }, cs.[0].Expr.Info.Type)
+
+[<Test>]
 let ``simple select with parameter not null`` () =
     let cmd = 
         seq {
