ad0c712afefb652235d6c1e05912e77504f9b409
Robert Peele <robert.s.peele@gmail.com>
2017-02-03 20:06:31 -0500

    Move tests for aggregate-checking into their own file.

---
+++ Rezoom.SQL.Test/TestAggregateErrors.fs
@@ -0,0 +1,31 @@
+﻿module Rezoom.SQL.Test.TestAggregateErrors
+open System
+open NUnit.Framework
+open FsUnit
+open Rezoom.SQL
+open Rezoom.SQL.Mapping
+
+[<Test>]
+let ``aggregates without group must not be found with non-aggregates`` () =
+    expectError Error.columnNotAggregated
+        """
+            select sum(Id) as Sum, Id from Users
+        """
+
+[<Test>]
+let ``aggregates with group by must not contain non-grouped column references`` () =
+    expectError Error.columnNotGroupedBy
+        """
+            select Id, Name
+            from Users
+            group by Id
+        """
+
+[<Test>]
+[<Ignore("fails right now, need an error for this")>]
+let ``aggregates may not appear in where clause`` () =
+    expectError ""
+        """
+            select count(*) as x from Users
+            where count(*) > 0
+        """
--- Rezoom.SQL.Test/TestTypeErrors.fs
+++ Rezoom.SQL.Test/TestTypeErrors.fs
@@ -64,22 +64,6 @@ let ``sum argument must be numeric`` () =
         """
 
 [<Test>]
-let ``aggregates without group must not be found with non-aggregates`` () =
-    expectError Error.columnNotAggregated
-        """
-            select sum(Id) as Sum, Id from Users
-        """
-
-[<Test>]
-let ``aggregates with group by must not contain non-grouped column references`` () =
-    expectError Error.columnNotGroupedBy
-        """
-            select Id, Name
-            from Users
-            group by Id
-        """
-
-[<Test>]
 let ``coalesce forces first arg nullable`` () =
     expectError Error.exprMustBeNullable
         """
