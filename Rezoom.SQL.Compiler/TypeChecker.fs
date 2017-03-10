0d805a504ad9e75355aa252235580888f97a3129
Robert Peele <robert.s.peele@gmail.com>
2017-03-09 20:27:57 -0500

    Use private type aliases in typechecker.

--- Rezoom.SQL.Compiler/TypeChecker.fs
+++ Rezoom.SQL.Compiler/TypeChecker.fs
@@ -3,8 +3,8 @@ open System
 open System.Collections.Generic
 open Rezoom.SQL.Compiler.InferredTypes
 
-type InferredQueryShape = InferredType QueryExprInfo
-type SelfQueryShape =
+type private InferredQueryShape = InferredType QueryExprInfo
+type private SelfQueryShape =
     // this thing is for when we know ahead of time what the column names of a select statement are supposed to be
     // so we don't want to require that they all be aliased manually.
     {   CTEName : Name option
