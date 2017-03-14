4d53115d64458fb0a70a3061ee6ba5dfb734ce13
Robert Peele <robert.s.peele@gmail.com>
2017-03-14 00:29:19 -0400

    Add Rezoom.SQL.Provider.TSQL package.

---
+++ NuGet/Rezoom.SQL.Provider.TSQL/content/V1.model.sql
@@ -0,0 +1,22 @@
+-- Edit this file to define your initial database model.
+
+create table Users
+    ( Id int primary key autoincrement
+    , Email string(254) unique
+    , Name string(64) null
+    , RegisteredUtc datetime
+    );
+
+create table Comments
+    ( Id int primary key autoincrement
+    , AuthorId int references Users(Id)
+    , CreatedUtc datetime
+    , Comment string(512)
+    );
+
+create index IX_Comments_AuthorId on Comments
+    (AuthorId);
+
+-- If you want to make model changes after you have created data, add
+-- the statements for those changes (e.g. ALTER TABLE) in another file
+-- called V2.yourchangetitle.sql.
