50ccc3f0663b0d1615ccf0cd38ccbc573580ddae
Robert Peele <robert.s.peele@gmail.com>
2017-03-16 01:07:32 -0400

    Add stub for documentation with F# formatting.

---
+++ Documentation/Tutorial.fsx
@@ -0,0 +1,35 @@
+﻿(*** hide ***)
+
+#r "../packages/Rezoom.0.2.1/lib/net46/Rezoom.dll"
+#r "../packages/Rezoom.SQL.Provider.0.1.2/lib/net46/Rezoom.SQL.Compiler.dll"
+#r "../packages/Rezoom.SQL.Provider.0.1.2/lib/net46/Rezoom.SQL.Mapping.dll"
+#r "../packages/Rezoom.SQL.Provider.0.1.2/lib/net46/Rezoom.SQL.Provider.dll"
+#nowarn "193"
+open System
+
+(**
+
+# Tutorial
+
+This tutorial will get you up and running with Rezoom.SQL. You'll need
+[Visual Studio 2017](https://www.visualstudio.com/downloads/) -- the free
+Community Edition is fine.
+
+Make sure you check the box for "F# language support" in the installer.
+If you've already installed VS2017, you can re-run the installer and modify
+your installation to include F# language support.
+
+*)
+
+open Rezoom.SQL.Provider
+open Rezoom.SQL.Synchronous
+
+type Q = SQL<"""
+    create table X(a int primary key autoincrement, b string(10));
+    select * from X;
+""">
+
+let xs = Q.Command().Execute(null)
+for x in xs do
+    printfn "%d %s" y.a y.b
+
---
+++ Documentation/build.fsx
@@ -0,0 +1,31 @@
+﻿#load "../packages/FSharp.Formatting.2.14.4/FSharp.Formatting.fsx"
+open FSharp.Literate
+open System.IO
+
+let source = __SOURCE_DIRECTORY__
+let output = Path.Combine(__SOURCE_DIRECTORY__, "bin/doc")
+let template = Path.Combine(source, "template.html")
+
+let copies =
+    [   "tips.js"
+        "style.css"
+    ]
+
+let inputs =
+    [   "Tutorial.fsx"
+    ]
+
+for input in inputs do
+    Literate.ProcessScriptFile
+        ( input = Path.Combine(source, input)
+        , output = Path.Combine(output, input.Replace(".fsx", ".html"))
+        , templateFile = template
+        , lineNumbers = false
+        , replacements =
+            [   "project-name", "Rezoom.SQL"
+                "github-link", "https://github.com/rspeele/Rezoom.SQL"
+            ]
+        )
+
+for copy in copies do
+    File.Copy(Path.Combine(source, copy), Path.Combine(output, copy), overwrite = true)
