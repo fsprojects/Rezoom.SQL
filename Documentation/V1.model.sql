788143b10d86f6ce72a473ce249d9ed157e31610
Robert Peele <robert.s.peele@gmail.com>
2017-03-19 18:37:13 -0400

    Add stuff about loading nested types to tutorial.

--- Documentation/V1.model.sql
+++ Documentation/V1.model.sql
@@ -13,3 +13,45 @@ create table Comments
 
 create index IX_Comments_AuthorId on Comments
     (AuthorId);
+
+
+create table Kingdoms
+	( Id int primary key autoincrement
+	, Name string(80)
+	);
+
+create table Phyla
+	( Id int primary key autoincrement
+	, KingdomId int references Kingdoms(Id)
+	, Name string(80)
+	);
+
+create table Classes
+	( Id int primary key autoincrement
+	, PhylumId int references Phyla(Id)
+	, Name string(80)
+	);
+
+create table Orders
+	( Id int primary key autoincrement
+	, ClassId int references Classes(Id)
+	, Name string(80)
+	);
+
+create table Families
+	( Id int primary key autoincrement
+	, OrderId int references Orders(Id)
+	, Name string(80)
+	);
+
+create table Genera
+	( Id int primary key autoincrement
+	, FamilyId int references Families(Id)
+	, Name string(80)
+	);
+
+create table Species
+	( Id int primary key autoincrement
+	, GenusId int references Genera(Id)
+	, Name string(80)
+	);
