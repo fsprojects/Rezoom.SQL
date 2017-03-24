0d61df1f928d172eaf74835f457b36d3f464268d
Robert Peele <robert.s.peele@gmail.com>
2017-03-24 19:28:33 -0400

    Add tests for creating the same view/table multiple times.

---
+++ src/Rezoom.SQL.Test/TestModelErrors.fs
@@ -0,0 +1,24 @@
+﻿module Rezoom.SQL.Test.TestModelErrors
+open System
+open NUnit.Framework
+open FsUnit
+open Rezoom.SQL.Compiler
+open Rezoom.SQL.Mapping
+
+[<Test>]
+let ``duplicate create view complains`` () =
+    expectError (Error.objectAlreadyExists "VUsers")
+        """
+            create view VUsers as
+                select * from Users;
+            create view VUsers as
+                select * from USers;
+        """
+
+[<Test>]
+let ``duplicate create table complains`` () =
+    expectError (Error.objectAlreadyExists "Users")
+        """
+            create view Users as
+                select * from Users;
+        """
