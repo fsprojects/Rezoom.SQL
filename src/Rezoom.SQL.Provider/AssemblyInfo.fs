f508a180624c5db5a0160da17be8969a7d35c710
Robert Peele <robert.s.peele@gmail.com>
2017-04-01 21:03:15 -0400

    Bump NuGet package version to 0.4.0.

--- src/Rezoom.SQL.Provider/AssemblyInfo.fs
+++ src/Rezoom.SQL.Provider/AssemblyInfo.fs
@@ -34,8 +34,8 @@ open System.Runtime.InteropServices
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [<assembly: AssemblyVersion("1.0.*")>]
-[<assembly: AssemblyVersion("0.3.3.*")>]
-[<assembly: AssemblyFileVersion("0.3.3.*")>]
+[<assembly: AssemblyVersion("0.4.0.*")>]
+[<assembly: AssemblyFileVersion("0.4.0.*")>]
 
 #if !DEBUG
 [<assembly: AssemblyKeyFile("../../../robert.peele.snk")>]
