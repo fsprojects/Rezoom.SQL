8ff9a86855acf177c9adc786b73b18c5ee0614f4
Robert Peele <robert.s.peele@gmail.com>
2017-07-22 00:38:08 -0400

    Remove useless open statements.

--- src/Rezoom.SQL.Test/TestReadWriteReferences.fs
+++ src/Rezoom.SQL.Test/TestReadWriteReferences.fs
@@ -1,8 +1,5 @@
 ﻿module Rezoom.SQL.Test.ReadWriteReferences
 open NUnit.Framework
-open FsUnit
-open Rezoom.SQL.Mapping
-open Rezoom.SQL.Mapping.CodeGeneration
 
 [<Test>]
 let ``deletes cascade`` () =
