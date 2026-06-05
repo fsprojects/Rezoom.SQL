ef3fccca7b8d2154d2149e59976b3ad4ec0f9077
Robert Peele <humbobst@gmail.com>
2026-06-04 22:45:27 -0400

    Make it possible for built-in types to go through userTypeLibrary resolution.
    This way you can override for example, the storage of DateTime for SQLite
    to an integer unix time if you prefer that to the default ISO8601 string.

--- src/Rezoom.SQL.Compiler/UserTypeResolutionPass.fs
+++ src/Rezoom.SQL.Compiler/UserTypeResolutionPass.fs
@@ -5,20 +5,36 @@ open Rezoom.SQL.Mapping
 // Simple dictionary lookup, no need to do inference here. Inference
 // then has the benefit of knowing the underlying for each user primitive.
 
+
+
 type private UserTypeResolutionPass(userTypeLibrary : UserTypeLibrary) =
     inherit ASTMapping<unit, unit, unit, unit>((fun _ -> ()), (fun _ -> ()))
+    static let clrTypeName (tName : TypeName) =
+        match tName with
+        | GuidTypeName -> typeof<System.Guid>.FullName
+        | StringTypeName _ -> typeof<System.String>.FullName
+        | BinaryTypeName _ -> typeof<System.Byte[]>.FullName
+        | IntegerTypeName Integer16 -> typeof<System.Int16>.FullName
+        | IntegerTypeName Integer32 -> typeof<System.Int32>.FullName
+        | IntegerTypeName Integer64 -> typeof<System.Int64>.FullName
+        | FloatTypeName Float32 -> typeof<System.Single>.FullName
+        | FloatTypeName Float64 -> typeof<System.Double>.FullName
+        | DecimalTypeName -> typeof<System.Decimal>.FullName
+        | BooleanTypeName -> typeof<System.Boolean>.FullName
+        | DateTimeTypeName -> typeof<System.DateTime>.FullName
+        | DateTimeOffsetTypeName -> typeof<System.DateTimeOffset>.FullName
+        | UnresolvedTypeName name -> name
+        | ResolvedUserType rty -> rty.Name
     override this.TypeName(typeName : TypeName WithSource) =
-        match typeName.Value with
-        | UnresolvedTypeName name ->
-            match userTypeLibrary.UserPrimitiveByName(name) with
-            | FoundType userType -> { typeName with Value = ResolvedUserType userType }
-            | NotFoundType [||] ->
-                failAt typeName.Source <| Error.typeNameNotFound name userTypeLibrary.Identity
-            | NotFoundType candidates ->
-                failAt typeName.Source <| Error.typeNameNotFoundButClose name candidates
-            | AmbiguousType candidates ->
-                let candidates = candidates |> Seq.map (fun t -> t.UserCLRType.FullName)
-                failAt typeName.Source <| Error.typeNameAmbiguous name candidates
+        match typeName.Value, userTypeLibrary.UserPrimitiveByName(clrTypeName typeName.Value) with
+        | _, FoundType userType -> { typeName with Value = ResolvedUserType userType }
+        | UnresolvedTypeName name, NotFoundType [||] ->
+            failAt typeName.Source <| Error.typeNameNotFound name userTypeLibrary.Identity
+        | UnresolvedTypeName name, NotFoundType candidates ->
+            failAt typeName.Source <| Error.typeNameNotFoundButClose name candidates
+        | UnresolvedTypeName name, AmbiguousType candidates ->
+            let candidates = candidates |> Seq.map (fun t -> t.UserCLRType.FullName)
+            failAt typeName.Source <| Error.typeNameAmbiguous name candidates
         | _ -> typeName
     override this.ResultColumns(resultColumns, topLevel : bool) =
         let resolveRowType (rowType : RowType WithSource) =
